cc77b81956c42ef5a19e205fb3b9cc34e3bd29e9
Jacopo <jakonigro@gmail.com>
2020-08-05 16:03:26 +0200

    snack3

---
+++ snack2/script.js
@@ -0,0 +1,16 @@
+// prima parola
+var primaParola = prompt("inserisci prima parola");
+// seconda parola
+var secondaParola = prompt("inserisci seconda parola");
+// calcolo lunghezza parole e stampa
+if(primaParola.length > secondaParola.length) {
+  document.getElementById("lunga").innerHTML = primaParola;
+  document.getElementById("corta").innerHTML = secondaParola;
+}
+else if (primaParola.length < secondaParola.length){
+  document.getElementById("lunga").innerHTML = secondaParola;
+  document.getElementById("corta").innerHTML = primaParola;
+}
+else {
+document.getElementById("corta").innerHTML = "le parole hanno la stessa lunghezza";
+}
